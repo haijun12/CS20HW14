54d99409fccc658ee83c138c02cbee49a6c49c14
haijun12 <79031887+haijun12@users.noreply.github.com>
2021-12-08 11:01:55 -0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,14 @@
+const express = require("express");
+
+const PORT = process.env.PORT || 3000;
+
+const app = express();
+app.get('', (req, res) => {
+    fs.readFile('index.html', function (err, txt) {
+        if (err) throw (err);
+        res.writeHead(200, {'Content-Type': 'text/html'});
+        res.write(txt);
+    });
+});
+
+app.listen(PORT);
